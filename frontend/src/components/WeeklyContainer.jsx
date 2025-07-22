febf0b67b33e42560e4ca1bd94ecfae06872b7b2
Kacper Borowski <borowski.cacper@gmail.com>
2025-07-22 17:59:49 -0400

    Grid Creation in progress

--- frontend/src/components/WeeklyContainer.jsx
+++ frontend/src/components/WeeklyContainer.jsx
@@ -1,41 +1,64 @@
 import React from "react";
-import TestScroll from "./TestScroll";
 
 const WeeklyContainer = () => {
   const weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  //   const weekdaysHead = weekdays.map((weekday) => (
-  //     <WeekdayText key={weekday} weekday={weekday} />
-  //   ));
-
   const weekdaysHead = weekdays.map((weekday) => (
     <div key={weekday}>
       <p className="font-mono font-extrabold text-2xl">{weekday}</p>
     </div>
   ));
+  const hours = [
+    "1 AM",
+    "2 AM",
+    "3 AM",
+    "4 AM",
+    "5 AM",
+    "6 AM",
+    "7 AM",
+    "8 AM",
+    "9 AM",
+    "10 AM",
+    "11 AM",
+    "12 PM",
+    "1 PM",
+    "2 PM",
+    "3 PM",
+    "4 PM",
+    "5 PM",
+    "6 PM",
+    "7 PM",
+    "8 PM",
+    "9 PM",
+    "10 PM",
+    "11 PM",
+  ];
+  const hoursSideBar = hours.map((hour) => (
+    <div key={hour} className="">
+      <p className="font-mono font-bold text-sm my-8 text-right mr-3">{hour}</p>
+    </div>
+  ));
+  const gridColumn = [...Array(7)].map((_, index) => (
+    <div key={index} className="border-1 bg-yellow-300 p-3 w-full"></div>
+  ));
+  //   const gridRow = [...Array(7)].map((_, index) => (
+  //     <div key={index + 10} className="border-1 bg-yellow-300 p-3 w-full">
+  //       <div className="flex "> {gridRow}</div>
+  //     </div>
+  //   ));
 
   return (
-    <div className="flex flex-col border-y-1 border-r-2 rounded-r-3xl w-15/16 h-200 overflow-hidden bg-indigo-50">
+    <div className="flex flex-col border-y-1 border-r-2 rounded-r-3xl w-15/16 h-150 overflow-hidden bg-indigo-50">
       <div className="h-1/10 flex flex-row bg-red-50">
         <div className="w-1/10 bg-amber-50 border-r-1"></div>
         <div className="w-9/10 flex flex-row justify-around items-center border-b-1">
           {weekdaysHead}
         </div>
       </div>
-      <div className="h-9/10 bg-amber-900">
-        <div className="h-250 flex flex-row overflow-scroll">
-          <div className="w-1/10 bg-amber-100">
-            {" "}
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
-              eum eveniet quaerat et temporibus. Aliquid, dolorum! Repellendus
-              labore, ab dolores sint eum mollitia, dignissimos dicta sed ea
-              delectus rem quibusdam?
-            </p>
-          </div>
-          <div className="w-9/10 bg-amber-400 ">
-            <div className="  m-6">
-              <TestScroll />
-            </div>
+      <div className="h-9/10 overflow-scroll">
+        <div className=" flex flex-row ">
+          <div className="w-1/10 ">{hoursSideBar}</div>
+          <div className="w-9/10 ">
+            <div className="flex "> {gridColumn}</div>
           </div>
         </div>
       </div>
