3127b03e5b758736635fb52f8a8849a9d91a1427
Kacper Borowski <borowski.cacper@gmail.com>
2025-08-25 21:37:29 -0400

    Started creating function to turn calendarEvents into .ics file

--- backend/college-semester.js
+++ backend/college-semester.js
@@ -1,4 +1,5 @@
 import { colleges } from "./db.js";
+import ical from "ical-generator";
 
 export function getColleges() {
   return Object.keys(colleges);
@@ -12,4 +13,30 @@ export function getSemesterDetails(college) {
     throw error;
   }
   return details;
+}
+
+export function calendarEventsToICS(college, calendarEvents) {
+  const details = colleges[college];
+  if (!details) {
+    const error = new Error("College not in database");
+    error.statusCode = 404;
+    throw error;
+  }
+  if (!calendarEvents) {
+    const error = new Error("Invalid Calendar Events");
+    error.statusCode = 400;
+    throw error;
+  }
+  const { semesterStart, semesterEnd, daysOff, daysMoved } = details;
+  const calendar = ical({ name: "Semester Schedule" });
+
+  for (let id in calendarEvents) {
+    let { className, occurences } = calendarEvents[id];
+    let { weekDay, time } = occurences;
+
+    calendar.createEvent({
+      id,
+      start: new Date(),
+    });
+  }
 }
