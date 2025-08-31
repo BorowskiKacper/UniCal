916394d1b5ca9881244f64f62220ed5b4496646a
Kacper Borowski <borowski.cacper@gmail.com>
2025-08-30 20:47:59 -0400

    Changed CORS policy to only accept specific URLs

--- backend/index.js
+++ backend/index.js
@@ -12,8 +12,20 @@ import {
 dotenv.config();
 const PORT = process.env.PORT; // 3000;
 
+const allowedOrigins = ["http://localhost:5173"];
+
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 app.use(express.json());
 
 const storage = multer.memoryStorage();
