be61565d5ad8e7a433db8228ec5cf481b627ee6b
Kacper Borowski <borowski.cacper@gmail.com>
2025-09-05 09:04:59 -0400

    Improved dark mode grid lines

--- frontend/src/components/HorizontalGridLines.jsx
+++ frontend/src/components/HorizontalGridLines.jsx
@@ -4,7 +4,7 @@ const HorizontalGridLines = () => {
       {[...Array(24)].map((_, index) => (
         <div
           key={`Item-${index}`}
-          className="border-b border-slate-700/30 last:border-b-0"
+          className="border-b border-slate-700/30 dark:border-slate-400/30 last:border-b-0"
         />
       ))}
     </div>
