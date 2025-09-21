1f0ac400b8ac074a33fcdfa592bb6542b3c6818a
Kacper Borowski <borowski.cacper@gmail.com>
2025-09-21 16:08:45 -0400

    Modified button to optionally disable lighting svg

--- frontend/src/components/SubmitButton.jsx
+++ frontend/src/components/SubmitButton.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-const SubmitButton = ({ text, onClick, isDisabled, isLoading = false }) => {
+const SubmitButton = ({
+  text,
+  onClick,
+  isDisabled,
+  disableLightning = false,
+  isLoading = false,
+}) => {
   return (
     <button
       className={`
@@ -9,7 +15,7 @@ const SubmitButton = ({ text, onClick, isDisabled, isLoading = false }) => {
           isDisabled || isLoading
             ? "bg-gray-300 text-gray-500 cursor-not-allowed dark:bg-slate-600/50 dark:text-slate-400"
             : `shadow-sm bg-[#FFC107] text-gray-950 hover:bg-amber-500 hover:text-black
-              dark:bg-emerald-400 dark:text-emerald-950 dark:hover:bg-green-400 hover:dark:text-black `
+              dark:bg-emerald-400 dark:text-emerald-950 dark:hover:bg-green-400 hover:dark:text-black`
         }
         focus:outline-none focus:ring-2 focus:ring-[#FFC107]/50 focus:ring-offset-2 focus:ring-offset-white dark:focus:ring-[#34D399]/50 dark:focus:ring-offset-[#18181B]
       `}
@@ -31,7 +37,7 @@ const SubmitButton = ({ text, onClick, isDisabled, isLoading = false }) => {
               d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
             />
           </svg>
-        ) : !isDisabled ? (
+        ) : !isDisabled && !disableLightning ? (
           <svg
             className="w-5 h-5 group-hover:rotate-12 transition-transform duration-300"
             fill="none"
