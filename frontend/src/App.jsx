266b550c061e9bd06db4d713686f6c064c9ef520
Kacper Borowski <borowski.cacper@gmail.com>
2025-11-10 14:04:38 -0500

    remove unused useRef import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import CalendarContainer from "./components/Calendar/Container";
 import UploadContainer from "./components/Upload/Container";
